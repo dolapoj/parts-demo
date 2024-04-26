f9cfff98a6d70b7d0418acd1f3eb71eb7d391f10
dolapo <dolapo.johnson@afripointgroup.com>
2024-04-26 13:33:40 +0100

    Landing page navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,8 +1,9 @@
-'use client'
+"use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
 import logo from "../../images/kinetic-parts-logo.png";
+import Link from "next/link";
 
 type NavBarProps = {
   userData: any; // Define the type of userData here
@@ -16,19 +17,38 @@ const NavBar: React.FC<NavBarProps> = ({ userData }) => {
           alt="logo"
           src={logo}
           placeholder="blur"
-          className={`w-24 sm:w-48`}
+          className={`w-24 sm:w-40`}
         />
       </div>
-      <div>
+      <div className="">
         <form>
-          <input
-            className="input-field"
-            placeholder="Search by product name "
-          />
+          <label className="input input-bordered flex items-center gap-2">
+            <input type="text" className="grow border-none focus:border-none placeholder:italic placeholder:text-slate-400" placeholder="Search by product name" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-4 h-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </label>
         </form>
       </div>
       <div className="">
-        <h4>Hi, {userData.first_name}</h4>
+        <div className="flex flex-row gap-4 justify-start mr-6">
+          <Link href="/login">
+            <span>Sign In</span>
+          </Link>
+          <Link href="/signup">
+            <span>Sign Up</span>
+          </Link>
+        </div>
+        {userData.first_name && <h4>Hi, {userData.first_name}</h4>}
         <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
             <div className="indicator">
@@ -46,7 +66,7 @@ const NavBar: React.FC<NavBarProps> = ({ userData }) => {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <span className="badge badge-sm indicator-item">2</span>
+              <span className="badge badge-sm indicator-item">0</span>
             </div>
           </div>
           <div
@@ -62,37 +82,39 @@ const NavBar: React.FC<NavBarProps> = ({ userData }) => {
             </div>
           </div>
         </div>
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <Image
-                alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
+        {userData.first_name && (
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+              </div>
             </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            {/* <li>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              {/* <li>
               <a>Settings</a>
             </li> */}
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-'use client'
-import React, { useState, useEffect } from "react";
-import NavBar from "./components/NavBar"
+"use client";
+import React, { useState, useEffect, Suspense } from "react";
+import NavBar from "./components/NavBar";
 
 type UserData = {
   first_name: string;
@@ -9,20 +9,42 @@ type UserData = {
 };
 
 export default function Home() {
-  const [userData, setUserData] = useState<UserData | any>({})
+  const [userData, setUserData] = useState<UserData | any>({});
 
   useEffect(() => {
     //Retrieve userData from sessionStorage when component mounts
-    const storedUserData = sessionStorage.getItem('userData');
+    const storedUserData = sessionStorage.getItem("userData");
     if (storedUserData) {
-      setUserData(JSON.parse(storedUserData))
+      setUserData(JSON.parse(storedUserData));
     }
-    console.log(storedUserData)
-  }, [])
+    console.log(storedUserData);
+  }, []);
+
+  const Loading = () => {
+    return (
+      <div className="min-h-screen flex flex-row justify-center items-center text-2xl">
+        <span className="loading loading-ring loading-xs"></span>
+        <span className="loading loading-ring loading-sm"></span>
+        <span className="loading loading-ring loading-md"></span>
+        <span className="loading loading-ring loading-lg"></span>
+      </div>
+    );
+  };
 
   return (
     <main className="">
-      <NavBar userData={userData} />
+      <Suspense fallback={Loading()}>
+        <NavBar userData={userData} />
+        <section className="bg-cyan-900 text-white font-semibold py-2 px-12">
+          <ul className="flex justify-start items-center gap-8">
+            <li className="cursor-pointer">Products</li>
+            <li className="cursor-pointer">Categories</li>
+            <li className="cursor-pointer">Brand</li>
+            <li className="cursor-pointer">Deals</li>
+            <li className="cursor-pointer">Customer Service</li>
+          </ul>
+        </section>
+      </Suspense>
     </main>
-  )
+  );
 }
