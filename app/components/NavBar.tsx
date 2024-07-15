67e7a5b512f8d86e14638401c61e9e17c9a8e158
dolapo <dolapo.johnson@afripointgroup.com>
2024-07-15 12:02:26 +0100

    Fix: NavBar Component

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -166,20 +166,6 @@ const NavBar: React.FC<NavBarProps> = () => {
                       <span className="badge badge-sm indicator-item">0</span>
                     </div>
                   </div>
-                  <div
-                    tabIndex={0}
-                    className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
-                  >
-                    <div className="card-body">
-                      <span className="font-bold text-lg">2 Items</span>
-                      <span className="text-info">Subtotal: $999</span>
-                      <div className="card-actions">
-                        <button className="btn btn-primary btn-block">
-                          View cart
-                        </button>
-                      </div>
-                    </div>
-                  </div>
                 </div>
                 {session || userData?.first_name ? (
                   <div className="dropdown dropdown-end">
